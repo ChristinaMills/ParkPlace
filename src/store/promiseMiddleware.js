0a74ad380f59bff2f1a17168768bbcb02d5cf825
G.Provost <grace.g.provost@gmail.com>
2018-04-21 11:12:13 -0700

    add protection to promise middleware for if there is no action

--- src/store/promiseMiddleware.js
+++ src/store/promiseMiddleware.js
@@ -3,6 +3,7 @@ import { LOAD_START, LOAD_END, ERROR } from '../components/app/reducers';
 const isPromise = val => val && typeof val.then === 'function';
 
 export default ({ dispatch }) => next => action => {
+  if(!action) return;
   const { type, payload } = action;
   if(!isPromise(payload)) return next(action);
 
